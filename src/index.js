6c3e364be8e8130fd4bc3225fcab6707bff4d047
xiaowusang55 <Xiaowusang55>
2019-09-01 23:58:28 +0800

    jsx

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './component/Tic-Tac-Toe/tic-tac-toe.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./component/Tic-Tac-Toe/tic-tac-toe.jsx";
 
-
-ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-);
-
-
-
+ReactDOM.render(<Game />, document.getElementById("root"));
